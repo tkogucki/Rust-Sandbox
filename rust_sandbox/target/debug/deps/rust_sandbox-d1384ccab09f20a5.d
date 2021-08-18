f4059d50bedb63c5b65e520a88c184771ca4bf5d
tkogucki <bpasssr.71@gmail.com>
2021-08-17 20:26:24 -0700

    Chapter 2 Hands on Project

---
+++ rust_sandbox/target/debug/deps/rust_sandbox-d1384ccab09f20a5.d
@@ -0,0 +1,5 @@
+/home/tim/Projects/Rust-Sandbox/rust_sandbox/target/debug/deps/rust_sandbox-d1384ccab09f20a5: src/main.rs
+
+/home/tim/Projects/Rust-Sandbox/rust_sandbox/target/debug/deps/rust_sandbox-d1384ccab09f20a5.d: src/main.rs
+
+src/main.rs:
---
+++ rust_sandbox/target/debug/rust_sandbox.d
@@ -0,0 +1,1 @@
+/home/tim/Projects/Rust-Sandbox/rust_sandbox/target/debug/rust_sandbox: /home/tim/Projects/Rust-Sandbox/rust_sandbox/src/main.rs
